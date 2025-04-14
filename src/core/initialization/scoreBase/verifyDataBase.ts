ae2fdb31728930ad2a6d157011b373d9f6418a70
PuppyOne <zheng_xz@qq.com>
2025-04-14 23:49:23 +0800

    refactor(verifyDataBase): 移除未使用的变量
    移除未使用的 `ScoreBaseSnapshot` 变量

--- src/core/initialization/scoreBase/verifyDataBase.ts
+++ src/core/initialization/scoreBase/verifyDataBase.ts
@@ -1,11 +1,7 @@
 import { ScoreboardObjective, world } from '@minecraft/server';
 import ScoreBase from './rw';
 
-let ScoreBaseSnapshot = <ScoreboardObjective[]>ScoreBase.GetObject();
-
-
 const verify = function () {
-    ScoreBaseSnapshot = world.scoreboard.getObjectives()
     world.scoreboard.getObjective('##FlashPlayer##') || world.scoreboard.addObjective('##FlashPlayer##')
 
     world.scoreboard
