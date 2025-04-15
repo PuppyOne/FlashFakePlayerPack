76c48c1de3883dabd2829eaf324df1c3523f5d71
PuppyOne <zheng_xz@qq.com>
2025-04-15 08:11:27 +0800

    refactor(verifyDataBase): 新增 `PIDManager` 类以迁移原始记分板功能
    - 重写 ScoreboardManager 类并导出，更名为 PIDManager
    - 新增 next 和 reset 方法，用于管理和重置 PID
    - 统一使用了 if-statement 进行空值判断，提高可读性
    - 删除未使用的导入

--- src/core/initialization/scoreBase/verifyDataBase.ts
+++ src/core/initialization/scoreBase/verifyDataBase.ts
@@ -1,15 +1,24 @@
-import { ScoreboardObjective, world } from '@minecraft/server';
-import ScoreBase from './rw';
+import { world } from '@minecraft/server';
 
-class ScoreboardManager {
-    initialize() {
-        world.scoreboard.getObjective('##FlashPlayer##') || world.scoreboard.addObjective('##FlashPlayer##');
+export class PIDManager {
+    constructor(private readonly initialValue: number = 1) {}
 
-        world.scoreboard
-            .getObjective('##FlashPlayer##')
-            .hasParticipant('##currentPID') ||
-            world.scoreboard.getObjective('##FlashPlayer##').setScore('##currentPID', 1);
+    initialize(): void {
+        if (!world.scoreboard.getObjective('##FlashPlayer##'))
+            world.scoreboard.addObjective('##FlashPlayer##');
+
+        if (!world.scoreboard.getObjective('##FlashPlayer##').hasParticipant('##currentPID'))
+            world.scoreboard.getObjective('##FlashPlayer##').setScore('##currentPID', this.initialValue);
+    }
+
+    next(): number {
+        const pid = world.scoreboard.getObjective('##FlashPlayer##').addScore('##currentPID', 1);
+        return pid;
+    }
+
+    reset(): number {
+        const currentPID = world.scoreboard.getObjective('##FlashPlayer##').getScore('##currentPID');
+        world.scoreboard.getObjective('##FlashPlayer##').setScore('##currentPID', this.initialValue);
+        return currentPID;
     }
 }
-
-export default new ScoreboardManager().initialize
