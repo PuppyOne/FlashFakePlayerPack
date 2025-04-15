6fa7b1aea81da3d81ab5255bd32a28df324f27b8
PuppyOne <zheng_xz@qq.com>
2025-04-15 09:02:58 +0800

    refactor(core): 重命名索引 verifyDataBase 文件为 manager & 更新导入
    将 src/core/initialization/scoreBase/verifyDataBase.ts 重命名为 src/core/pid/manager.ts
    
    同步更新 `main.ts` 的导入

--- src/plugins/main.ts
+++ src/plugins/main.ts
@@ -9,7 +9,7 @@ import {Dimension, LocationOutOfWorldBoundariesError, system, Vector3} from '@mi
 
 import { register } from '@minecraft/server-gametest'
 
-import { PIDManager } from '../core/initialization/scoreBase/verifyDataBase'
+import { PIDManager } from '../core/pid/manager'
 import EventSignal from '../lib/xboyEvents/EventSignal'
 
 import { SIGN } from '../lib/xboyPackage/YumeSignEnum'
