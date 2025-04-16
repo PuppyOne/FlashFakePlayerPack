f40f2630347847b742568e8fd14e19b9b1e535cb
PuppyOne <zheng_xz@qq.com>
2025-04-16 20:52:52 +0800

    refactor: 补充可选值类型

--- src/core/test/manager.ts
+++ src/core/test/manager.ts
@@ -5,19 +5,19 @@ const overworld = world.getDimension('overworld');
 
 export class TestManager {
     private readonly maxTicks = 20 * 60 * 60 * 24 * 365;
-    private _testLocation: Vector3;
+    private _testLocation: Vector3 | undefined;
     private _test: Test | undefined;
 
-    private _resolve: (value: Test | PromiseLike<Test>) => void;
+    private _resolve: ((value: Test | PromiseLike<Test>) => void) | undefined;
     public readonly ready = new Promise<Test>(resolve => {
         this._resolve = resolve;
     });
 
-    get testLocation(): Vector3 {
+    get testLocation(): Vector3 | undefined {
         return this._testLocation;
     }
 
-    get test(): Test {
+    get test(): Test | undefined {
         return this._test;
     }
 
