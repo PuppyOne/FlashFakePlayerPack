2b66e2b2b7d21ba39bb296a50528adf8b79838a0
PuppyOne <zheng_xz@qq.com>
2025-04-16 10:26:07 +0800

    refactor(core/queries): 删除部分注释

--- src/core/queries/Util.ts
+++ src/core/queries/Util.ts
@@ -10,35 +10,8 @@ export const getSimPlayer = {
 
 }
 
-
-// function getEntitiesNear(who:Entity|Block,maxDistance:number,defEntityQueryOptions:EntityQueryOptions){
-//     const EQO : EntityQueryOptions = {}//new EntityQueryOptions()
-//     EQO.maxDistance = maxDistance
-//     EQO.location    = who.location
-//     EQO.excludeTypes= ["minecraft:player","minecraft:arrow","minecraft:xp_orb","minecraft:item"]
-//     EQO.closest   = 1
-//     Object.assign(EQO,defEntityQueryOptions)
-//     const entities = who.dimension.getEntities(EQO)
-//     const targets = []
-//     for(const entity of entities)targets.push(entity)
-//     return targets
-// }
-// function getPlayerNear(who:Entity|Block,maxDistance:number,defEntityQueryOptions:EntityQueryOptions){
-//     const EQO: EntityQueryOptions = {}//new EntityQueryOptions();
-//     EQO.maxDistance = maxDistance;                                                               ;;"距离";;
-//     EQO.location    = who.location;  //new Location(逻辑主体.location.x,逻辑主体.location.y,逻辑主体.location.z);                                                  ;;"中心坐标-ri泥god";;
-//     // EQO.excludeTypes= ["minecraft:arrow","minecraft:xp_orb","minecraft:item"];                                              ;;"排除掉的实体类型";;
-//     EQO.closest   = 1;
-//     EQO.excludeTags = [SIGN.YUME_SIM_SIGN]
-//     Object.assign(EQO,defEntityQueryOptions)
-//     const entities = who.dimension.getEntities(EQO)
-//     const targets = []
-//     for(const entity of entities)targets.push(entity)
-//     return targets
-// }
-
 export function getEntitiesNear(location:Vector3, dimension:Dimension, maxDistance:number, Options={}){
-    const EntityQueryOption:EntityQueryOptions = {}//new EntityQueryOptions()
+    const EntityQueryOption:EntityQueryOptions = {}
     EntityQueryOption.maxDistance = maxDistance
     EntityQueryOption.location    = location
     EntityQueryOption.excludeTypes= ["minecraft:player","minecraft:arrow","minecraft:xp_orb","minecraft:item"]
@@ -47,7 +20,7 @@ export function getEntitiesNear(location:Vector3, dimension:Dimension, maxDistan
     return dimension.getEntities(EntityQueryOption)
 }
 export function getPlayerNear(who:Entity|Block, maxDistance:number, defEntityQueryOptions:EntityQueryOptions):Player[] {
-    const EQO: EntityQueryOptions = {}//new EntityQueryOptions()
+    const EQO: EntityQueryOptions = {}
     EQO.maxDistance = maxDistance
     EQO.location    = who.location
     // EQO.excludeTypes= ["minecraft:arrow","minecraft:xp_orb","minecraft:item"]
