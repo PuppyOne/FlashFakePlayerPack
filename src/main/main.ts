3ca3a97bab5d8b95039be399a6e244f25a51670f
PuppyOne <zheng_xz@qq.com>
2025-04-16 13:50:11 +0800

    refactor(src): 优化 `main/` 和 `plugins/` 目录 & 更新导入
    - 将 `main.ts` 从 `plugins/` 目录移动到上级 `src/目录
    - 更新相关文件的导入路径
    - 新增 `plugins/index.ts` 文件，统一导出插件功能
    - 删除 `main/main.ts` 文件中的冗余代码
    - 更新文件中的导入路径

--- src/main/main.ts
+++ src/main/main.ts
@@ -2,26 +2,10 @@
 import tpsShower  from '../plugins/tpsShower';
 
 
-import "../plugins/main.js";
-import { world, system } from "@minecraft/server";
-const overworld = world.getDimension("overworld");
-const nether = world.getDimension("nether");
-const the_end = world.getDimension("the end");
-const nowTimeTemp    = new Date().getDate() +"日"+ new Date().getHours() +"时"+ new Date().getMinutes() +"分"+ new Date().getSeconds() +"秒"
+import "../main.js";
+import { system } from "@minecraft/server";
 
-// world.events.reloadFromCmd.subscribe(()=>{
-//        console.error("你没事reload干嘛呢？")
-//        overworld.runCommand("me 你没事reload干嘛呢？")
-// })
-
-system.runInterval(
-       () => {
-              tpsShower()
-              // let i = overworld.runCommandAsync("list")
-              // overworld.runCommandAsync("me "+JSON.stringify(i))
-              // overworld.runCommandAsync("me #########")
-       }
-);//笑死，祖传代码-2023-5-31
+system.runInterval(tpsShower);//笑死，祖传代码-2023-5-31
 
 
 // 🟧🟧🟧🟧🟧🟧🟧🟧
