f303bdb97aa52f3ea46c799babfaed12bc7c9ab2
PuppyOne <zheng_xz@qq.com>
2025-04-17 12:41:50 +0800

    refactor(events/move): 优化代码格式

--- src/lib/xboyEvents/ready.ts
+++ src/lib/xboyEvents/ready.ts
@@ -1,33 +1,33 @@
-import { type Player, system,world} from "@minecraft/server"
+import { type Player, system, world } from "@minecraft/server";
 import EventSignal from "./EventSignal";
 import type { playerReadyAfterEventSignal } from "../../@types/globalThis";
 
 // EventSignal
-export const playerReady:playerReadyAfterEventSignal = new EventSignal<undefined>()
+export const playerReady: playerReadyAfterEventSignal = new EventSignal<undefined>();
 
-const playerViewYMap = new Map<Player, number>()
+const playerViewYMap = new Map<Player, number>();
 
-const update = ()=>{
-    const playerList = world.getAllPlayers()
-          playerList.forEach(player=>{
-              const {y: currentViewY} = player.getViewDirection()
-              
-              const storedViewY = playerViewYMap.get(player)
-              
-              if(storedViewY===undefined)
-                // set to Map
-            return playerViewYMap.set(player, currentViewY)
-            
-            if(storedViewY==currentViewY)
-                // nothing
-                return
-            
-            // update to Map && Event-trigger
-            playerViewYMap.set(player, currentViewY)
-              playerReady.trigger(undefined)
+const update = () => {
+    const playerList = world.getAllPlayers();
+    playerList.forEach(player => {
+        const { y: currentViewY } = player.getViewDirection();
 
-              system.clearRun(id)
-          })
-}
+        const storedViewY = playerViewYMap.get(player);
 
-const id=system.runInterval(update,4)
+        if (storedViewY === undefined)
+            // set to Map
+            return playerViewYMap.set(player, currentViewY);
+
+        if (storedViewY == currentViewY)
+            // nothing
+            return;
+
+        // update to Map && Event-trigger
+        playerViewYMap.set(player, currentViewY);
+        playerReady.trigger(undefined);
+
+        system.clearRun(id);
+    });
+};
+
+const id = system.runInterval(update, 4);
