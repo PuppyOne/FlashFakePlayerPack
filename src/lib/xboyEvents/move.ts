b91cf16f683a195aa92fa59879b8ef1c1926c72a
PuppyOne <zheng_xz@qq.com>
2025-04-17 12:22:00 +0800

    refactor(events/move): 简化 `PlayerMove` 事件
    - 只观测 y 视角
    - 删去 `PlayerMove` 事件的信息对象 现在是无参的，由于信息未使用
    - 移除快照相关的功能

--- src/lib/xboyEvents/move.ts
+++ src/lib/xboyEvents/move.ts
@@ -1,40 +1,34 @@
 import { type Player, system,type Vector3, world} from "@minecraft/server"
 import EventSignal from "./EventSignal";
-import type { playerMoveAfterEvent, playerMoveAfterEventSignal } from "../../@types/globalThis";
+import type { playerMoveAfterEventSignal } from "../../@types/globalThis";
 
 
 
 // EventSignal
-export const playerMove:playerMoveAfterEventSignal = new EventSignal<playerMoveAfterEvent>()
+export const playerMove:playerMoveAfterEventSignal = new EventSignal<undefined>()
 // console.error(JSON.stringify(world.getAllPlayers()[0].getViewDirection()))
 
-const playerInfo = new Map<Player,{location:Vector3,viewDirection: Vector3,flash:string}>()
-
-const getFlash = ({x,y,z}:Vector3)=>    String(x)+String(y)+String(z)
-
+const playerViewYMap = new Map<Player, number>()
 
 const update = ()=>{
     const playerList = world.getAllPlayers()
           playerList.forEach(player=>{
-              const location = player.location
-              const viewDirection = player.getViewDirection()
-              const flash = getFlash(location)+getFlash(viewDirection)
-              const lastInfo = playerInfo.get(player)
+              const {y: currentViewY} = player.getViewDirection()
+              
+              const storedViewY = playerViewYMap.get(player)
+              
+              if(storedViewY===undefined)
+                // set to Map
+            return playerViewYMap.set(player, currentViewY)
+            
+            if(storedViewY==currentViewY)
+                // nothing
+                return
+            
+            // update to Map && Event-trigger
+            playerViewYMap.set(player, currentViewY)
+              playerMove.trigger(undefined)
 
-
-              if(lastInfo===undefined)
-                  // set to Map
-                  return playerInfo.set(player,{location,viewDirection,flash})
-
-              if(lastInfo.flash==flash)
-                  // nothing
-                  return void flash
-
-              // update to Map && Event-trigger
-              playerInfo.set(player,{location,viewDirection,flash})
-              const {location:locationBefore,viewDirection:viewDirectionBefore} = playerInfo.get(player)
-              playerMove.trigger({location, viewDirection,locationBefore,viewDirectionBefore})
-         
               system.clearRun(id)
           })
 }
