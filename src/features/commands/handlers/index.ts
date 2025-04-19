3a1dbc9a74c73c7b824e49d125a6c1d68e217ebc
PuppyOne <zheng_xz@qq.com>
2025-04-19 22:52:43 +0800

    refactor: 调整 `index.ts` 配置使其更清晰规整

---
+++ src/features/commands/handlers/index.ts
@@ -0,0 +1,20 @@
+export * from './meta/help';
+export * from './meta/list';
+export * from './meta/location';
+export * from './meta/rename';
+export * from './meta/setting';
+export * from './meta/showCommandsList';
+export * from './meta/time';
+
+export * from './lifecycle/chatSpawn';
+export * from './lifecycle/disconnect';
+export * from './lifecycle/respawn';
+
+export * from './inventory/equipment-swap';
+export * from './inventory/inventory-swap';
+export * from './inventory/mainhand-swap';
+export * from './inventory/offhand-swap';
+export * from './inventory/recycle';
+
+export * from './behaviors/behaviors';
+export * from './behaviors/breakBlock';
