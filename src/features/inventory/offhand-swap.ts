5eba4ca5b059d1b029368a76cca51a8e9d78977a
PuppyOne <zheng_xz@qq.com>
2025-05-07 14:41:14 +0800

    refactor(inventory): 移除部分多余空行

--- src/features/inventory/offhand-swap.ts
+++ src/features/inventory/offhand-swap.ts
@@ -5,7 +5,6 @@ import type { SimulatedPlayer } from "@minecraft/server-gametest";
 import { swapEquipment } from "./utils";
 
 commandManager.add('假人副手物品交换', ({player,simulatedPlayer: sim}) => {
-
     const simulatedPlayer:SimulatedPlayer = sim || getSimPlayer.fromView(player)
 
     swapEquipment(player, simulatedPlayer, EquipmentSlot.Offhand)
