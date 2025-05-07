844483c24ffd7be829318c8ac499b4dff117d10a
PuppyOne <zheng_xz@qq.com>
2025-05-07 14:15:43 +0800

    refactor(inventory): 调整 `swapEquipment` 函数内语句顺序

--- src/features/inventory/utils.ts
+++ src/features/inventory/utils.ts
@@ -1,12 +1,12 @@
 import { EntityComponentTypes, type EquipmentSlot, type Player } from "@minecraft/server";
 
 export const swapEquipment = (playerA: Player, playerB: Player, slot: EquipmentSlot): void => {
-    const componentB = playerB.getComponent(EntityComponentTypes.Equippable);
     const componentA = playerA.getComponent(EntityComponentTypes.Equippable);
+    const componentB = playerB.getComponent(EntityComponentTypes.Equippable);
 
-    const itemB = componentB.getEquipment(slot);
     const itemA = componentA.getEquipment(slot);
+    const itemB = componentB.getEquipment(slot);
 
-    componentB.setEquipment(slot, itemA);
     componentA.setEquipment(slot, itemB);
+    componentB.setEquipment(slot, itemA);
 };
