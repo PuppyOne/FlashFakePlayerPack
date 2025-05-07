f7b2b57a5aa63e5d81baebb6f3b301d5fd44dd38
PuppyOne <zheng_xz@qq.com>
2025-05-07 14:15:14 +0800

    refactor(inventory): `swapEquipment` 函数内局部变量名语义化 提高可读性

--- src/features/inventory/utils.ts
+++ src/features/inventory/utils.ts
@@ -1,12 +1,12 @@
 import { EntityComponentTypes, type EquipmentSlot, type Player } from "@minecraft/server";
 
 export const swapEquipment = (playerA: Player, playerB: Player, slot: EquipmentSlot): void => {
-    const s = playerB.getComponent(EntityComponentTypes.Equippable);
-    const p = playerA.getComponent(EntityComponentTypes.Equippable);
+    const componentB = playerB.getComponent(EntityComponentTypes.Equippable);
+    const componentA = playerA.getComponent(EntityComponentTypes.Equippable);
 
-    const _ = s.getEquipment(slot);
-    const __ = p.getEquipment(slot);
+    const itemB = componentB.getEquipment(slot);
+    const itemA = componentA.getEquipment(slot);
 
-    s.setEquipment(slot, __);
-    p.setEquipment(slot, _);
+    componentB.setEquipment(slot, itemA);
+    componentA.setEquipment(slot, itemB);
 };
