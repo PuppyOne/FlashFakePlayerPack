c97b498a3312b8359049c0eaf4de66499fb4cb28
PuppyOne <zheng_xz@qq.com>
2025-05-17 20:48:14 +0800

    refactor(features/reset-pid): 更改文案为“假人”
    与上下文相统一

--- src/features/meta/reset-pid.ts
+++ src/features/meta/reset-pid.ts
@@ -10,7 +10,7 @@ commandManager.add(['假人重置序号', '假人编号重置', '假人序号重
         player?.sendMessage('以前是以前✋ ，现在是现在✋ ，你要是一直拿以前当作现在✋ ，哥们，你怎么不拿你开新档的时候对比')
     } catch (e) {
         if (e instanceof ExistingSimulatedPlayersError)
-            player?.sendMessage('请先移除所有模拟玩家');
+            player?.sendMessage('请先移除所有假人');
         else
             throw e;
     }
