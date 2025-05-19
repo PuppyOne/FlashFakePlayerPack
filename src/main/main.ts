eb6915af4e6bc576079279c80e08680c5bc88de5
PuppyOne <zheng_xz@qq.com>
2025-05-19 23:04:40 +0800

    refactor(main): 改用 `await` 语法 提高可读性

--- src/main/main.ts
+++ src/main/main.ts
@@ -6,16 +6,13 @@ import {playerReady} from "@/features/events";
 import { simulatedPlayerManager } from '@/core/simulated-player';
 import { gameTestManager } from '@/core/gametest';
 
-
-gameTestManager.ready.then(test => {
-    simulatedPlayerManager.test = test;
-    console.log('[模拟玩家] 初始化完成，输入“假人创建”或“ffpp”');
-});
-
-playerReady.then(async () => {
-    await gameTestManager.ready;
-    world.sendMessage('[模拟玩家] 初始化完成，输入“假人创建”或“ffpp”');
-});
-
 simulatedPlayerManager.initialize();
 gameTestManager.initialize();
+
+const test = await gameTestManager.ready;
+
+simulatedPlayerManager.test = test;
+console.log('[模拟玩家] 初始化完成，输入“假人创建”或“ffpp”');
+
+await playerReady;
+world.sendMessage('[模拟玩家] 初始化完成，输入“假人创建”或“ffpp”');
