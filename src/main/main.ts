80e9c12498fc1cf2a09deabf1bb544df53708310
PuppyOne <zheng_xz@qq.com>
2025-05-21 15:31:52 +0800

    refactor(main): `try` 块只包含最小部分
    利用 `throw` 阻止错误情况下的后续代码执行

--- src/main/main.ts
+++ src/main/main.ts
@@ -11,11 +11,12 @@ simulatedPlayerManager.initialize();
 try {
     const test = await gameTestManager.initialize();
     simulatedPlayerManager.test = test;
-    console.log('[模拟玩家] 初始化完成，输入“假人创建”或“ffpp”');
-
-    await playerReady;
-    world.sendMessage('[模拟玩家] 初始化完成，输入“假人创建”或“ffpp”');
-
 } catch (e) {
     world.sendMessage('[模拟玩家] 报错了，我也不知道为什么' + e);
+    throw e;
 }
+
+console.log('[模拟玩家] 初始化完成，输入“假人创建”或“ffpp”');
+
+await playerReady;
+world.sendMessage('[模拟玩家] 初始化完成，输入“假人创建”或“ffpp”');
