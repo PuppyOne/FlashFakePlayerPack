fb2f7457212be48700946e43b498f0b4410a4c65
PuppyOne <zheng_xz@qq.com>
2025-05-25 12:31:45 +0800

    refactor(behaviors): 形参直接使用解构

--- src/features/behaviors/behaviors.ts
+++ src/features/behaviors/behaviors.ts
@@ -72,13 +72,11 @@ const behaviorCommandConfigs: BehaviorCommandConfig[] = [
 const behaviorPrefix = '假人';
 
 const registerBehaviorCommands = () => {
-    behaviorCommandConfigs.forEach(behaviorCommand => {
-        commandManager.register(`${behaviorPrefix}${behaviorCommand.behavior}`, ({ player }) => {
+    behaviorCommandConfigs.forEach(({ behavior, adds, removes }) => {
+        commandManager.register(`${behaviorPrefix}${behavior}`, ({ player }) => {
             if (!player) return;
             const simulatedPlayer = getSimPlayer.fromView(player);
             if (!simulatedPlayer) return;
-
-            const { adds, removes } = behaviorCommand;
 
             adds.forEach(tag => {
                 simulatedPlayer.addTag(tag);
