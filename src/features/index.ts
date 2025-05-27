6bc3256eca6e3cef714446ad024dd4b4fe704f08
PuppyOne <zheng_xz@qq.com>
2025-05-27 11:35:10 +0800

    refactor(features): 封装函数 `initialize()` 自动递归导入功能模块 (#105)
    使用 webpack API 加载所有功能模块，避免手动维护列表
    
    - 移除 `features` 目录下所有硬编码的模块导出
    - 使用 webpack API 动态模块加载
    - 封装为函数 `initialize()` 通过调用该函数显式控制加载时机
    - 移除文件 `main/main.ts`

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,30 +1,3 @@
-export * from './task';
-export * from './gui';
-export * from './killedBySimPlayer';
+const context = require.context('.', true, /\.ts$/);
 
-export * from './command-triggers';
-
-export * from './meta/help';
-export * from './meta/list-simulated-players';
-export * from './meta/location';
-export * from './meta/rename';
-export * from './meta/reset-pid';
-export * from './meta/list-commands';
-export * from './meta/time';
-
-export * from './lifecycle/spawn';
-export * from './lifecycle/disconnect';
-export * from './lifecycle/respawn';
-
-export * from './inventory/equipment-swap';
-export * from './inventory/inventory-swap';
-export * from './inventory/mainhand-swap';
-export * from './inventory/offhand-swap';
-export * from './inventory/recycle';
-
-export * from './behaviors/behaviors';
-export * from './behaviors/break-block';
-
-export * from './tps'
-
-export * from './initialization/initialize';
+export const initialize = () => context.keys().map(context);
