8c2342f6286711440bcb3159face46744549f43e
PuppyOne <zheng_xz@qq.com>
2025-05-31 22:33:06 +0800

    refactor(queries): 改为 `as` 断言
    而不是 `<>` 断言

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -4,7 +4,7 @@ import { SIGN } from '@/constants'
 
 
 export function getSimulatedPlayerFromView(e: Entity, maxDistance = 16): SimulatedPlayer {
-    return (<SimulatedPlayer>e.getEntitiesFromViewDirection({ maxDistance, tags: [SIGN.YUME_SIM_SIGN] })[0]?.entity);
+    return e.getEntitiesFromViewDirection({ maxDistance, tags: [SIGN.YUME_SIM_SIGN] })[0]?.entity as SimulatedPlayer;
 }
 
 export function getEntitiesNear(location:Vector3, dimension:Dimension, maxDistance:number, Options={}){
