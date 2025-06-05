15d480f97fdcf801ca3a818f2232bc7d90d320ed
PuppyOne <zheng_xz@qq.com>
2025-06-05 09:04:38 +0800

    refactor(queries): 将函数声明改为箭头函数形式
    改为 `const` + 箭头函数形式

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -3,11 +3,11 @@ import {SimulatedPlayer} from '@minecraft/server-gametest'
 import { SIGN } from '@/constants'
 
 
-export function getSimulatedPlayerFromView(e: Entity, maxDistance = 16): SimulatedPlayer {
+export const getSimulatedPlayerFromView = (e: Entity, maxDistance = 16): SimulatedPlayer => {
     return e.getEntitiesFromViewDirection({ maxDistance, tags: [SIGN.YUME_SIM_SIGN] })[0]?.entity as SimulatedPlayer;
 }
 
-export function getClosestMob(location:Vector3, dimension:Dimension, maxDistance:number, Options={}): Entity{
+export const getClosestMob = (location:Vector3, dimension:Dimension, maxDistance:number, Options={}): Entity =>{
     return dimension.getEntities({
         excludeTypes: ["minecraft:player", "minecraft:arrow", "minecraft:xp_orb", "minecraft:item"],
         closest: 1,
@@ -16,7 +16,7 @@ export function getClosestMob(location:Vector3, dimension:Dimension, maxDistance
         ...Options,
     })[0];
 }
-export function getClosestPlayer(who:Entity|Block, maxDistance:number, defEntityQueryOptions:EntityQueryOptions={}):Player {
+export const getClosestPlayer = (who:Entity|Block, maxDistance:number, defEntityQueryOptions:EntityQueryOptions={}):Player => {
     return who.dimension.getPlayers({
         excludeTags: [SIGN.YUME_SIM_SIGN],
         closest: 1,
