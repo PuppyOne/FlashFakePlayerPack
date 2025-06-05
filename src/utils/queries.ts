028d6e8264a16eab9d301ee000c37892ec76f650
PuppyOne <zheng_xz@qq.com>
2025-06-05 09:17:42 +0800

    refactor(queries): 优化代码格式 避免单行过长
    增加换行以避免单行过长

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -1,14 +1,35 @@
 import { SIGN } from '@/constants';
-import type { Dimension, Entity, EntityQueryOptions, Player, Vector3 } from '@minecraft/server';
+import type {
+    Dimension,
+    Entity,
+    EntityQueryOptions,
+    Player,
+    Vector3,
+} from '@minecraft/server';
 import type { SimulatedPlayer } from '@minecraft/server-gametest';
 
-export const getSimulatedPlayerFromView = (e: Entity, maxDistance = 16): SimulatedPlayer => {
-    return e.getEntitiesFromViewDirection({ maxDistance, tags: [SIGN.YUME_SIM_SIGN] })[0]?.entity as SimulatedPlayer;
+export const getSimulatedPlayerFromView = (
+    e: Entity,
+    maxDistance = 16
+): SimulatedPlayer => {
+    return e.getEntitiesFromViewDirection({
+        maxDistance,
+        tags: [SIGN.YUME_SIM_SIGN],
+    })[0]?.entity as SimulatedPlayer;
 };
 
-export const getClosestMob = ({ dimension, location }: HasDimensionLocation, maxDistance: number, Options = {}): Entity => {
+export const getClosestMob = (
+    { dimension, location }: HasDimensionLocation,
+    maxDistance: number,
+    Options = {}
+): Entity => {
     return dimension.getEntities({
-        excludeTypes: ["minecraft:player", "minecraft:arrow", "minecraft:xp_orb", "minecraft:item"],
+        excludeTypes: [
+            'minecraft:player',
+            'minecraft:arrow',
+            'minecraft:xp_orb',
+            'minecraft:item',
+        ],
         closest: 1,
         location,
         maxDistance,
@@ -16,7 +37,11 @@ export const getClosestMob = ({ dimension, location }: HasDimensionLocation, max
     })[0];
 };
 
-export const getClosestPlayer = ({ dimension, location }: HasDimensionLocation, maxDistance: number, defEntityQueryOptions: EntityQueryOptions = {}): Player => {
+export const getClosestPlayer = (
+    { dimension, location }: HasDimensionLocation,
+    maxDistance: number,
+    defEntityQueryOptions: EntityQueryOptions = {}
+): Player => {
     return dimension.getPlayers({
         excludeTags: [SIGN.YUME_SIM_SIGN],
         closest: 1,
