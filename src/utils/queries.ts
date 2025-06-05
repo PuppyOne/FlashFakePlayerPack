739aa446932e7e9ab9669e78461021867758bb2e
PuppyOne <zheng_xz@qq.com>
2025-06-05 09:05:34 +0800

    refactor(queries): 调整导入顺序并优化代码格式

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -1,13 +1,12 @@
-import type {Block, Dimension, Entity, EntityQueryOptions, Player, Vector3} from '@minecraft/server'
-import {SimulatedPlayer} from '@minecraft/server-gametest'
-import { SIGN } from '@/constants'
-
+import { SIGN } from '@/constants';
+import type { Block, Dimension, Entity, EntityQueryOptions, Player, Vector3 } from '@minecraft/server';
+import type { SimulatedPlayer } from '@minecraft/server-gametest';
 
 export const getSimulatedPlayerFromView = (e: Entity, maxDistance = 16): SimulatedPlayer => {
     return e.getEntitiesFromViewDirection({ maxDistance, tags: [SIGN.YUME_SIM_SIGN] })[0]?.entity as SimulatedPlayer;
-}
+};
 
-export const getClosestMob = (location:Vector3, dimension:Dimension, maxDistance:number, Options={}): Entity =>{
+export const getClosestMob = (location: Vector3, dimension: Dimension, maxDistance: number, Options = {}): Entity => {
     return dimension.getEntities({
         excludeTypes: ["minecraft:player", "minecraft:arrow", "minecraft:xp_orb", "minecraft:item"],
         closest: 1,
@@ -15,8 +14,9 @@ export const getClosestMob = (location:Vector3, dimension:Dimension, maxDistance
         maxDistance,
         ...Options,
     })[0];
-}
-export const getClosestPlayer = (who:Entity|Block, maxDistance:number, defEntityQueryOptions:EntityQueryOptions={}):Player => {
+};
+
+export const getClosestPlayer = (who: Entity | Block, maxDistance: number, defEntityQueryOptions: EntityQueryOptions = {}): Player => {
     return who.dimension.getPlayers({
         excludeTags: [SIGN.YUME_SIM_SIGN],
         closest: 1,
@@ -24,4 +24,4 @@ export const getClosestPlayer = (who:Entity|Block, maxDistance:number, defEntity
         maxDistance,
         ...defEntityQueryOptions,
     })[0];
-}
+};
