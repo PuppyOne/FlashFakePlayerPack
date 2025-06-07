279724a7df9d594397eed5938c5fc877653f732a
PuppyOne <zheng_xz@qq.com>
2025-06-07 21:49:32 +0800

    refactor(features/inventory): 抽离 swapItems 函数 (#113)
    - 抽离 `swapItems` 函数到 `utils` 文件
    - 背包遍历方向由逆向遍历改为正向遍历
    - 优化代码，提高清晰度

--- src/features/inventory/inventory-swap.effect.ts
+++ src/features/inventory/inventory-swap.effect.ts
@@ -1,30 +1,12 @@
 import { commandManager } from "@/core/command";
 import { getSimulatedPlayerFromView } from "@/utils";
-import { EntityComponentTypes } from "@minecraft/server";
 import type { SimulatedPlayer } from "@minecraft/server-gametest";
+import { swapItems } from "./utils";
 
 commandManager.register(['假人背包交换','假人交换背包'], ({player,simulatedPlayer: sim}) => {
     if(!player && !sim)return
     const simulatedPlayer:SimulatedPlayer = sim || getSimulatedPlayerFromView(player)
     if(!simulatedPlayer)return
 
-    const s = simulatedPlayer.getComponent(EntityComponentTypes.Inventory).container
-
-    const p = player.getComponent(EntityComponentTypes.Inventory).container
-
-    for
-    (
-        let i = p.size;
-        i--;
-        s.getItem(i)
-            ?
-            p.getItem(i)
-                ? s.swapItems(i, i, p)
-                : s.moveItem(i, i, p)
-            :
-            p.getItem(i)
-                ? p.moveItem(i, i, s)
-                : "这行代码，我再维护我是狗"
-    ) ;
-
+    swapItems(simulatedPlayer, player);
 });
--- src/features/inventory/utils.ts
+++ src/features/inventory/utils.ts
@@ -1,5 +1,23 @@
 import { EntityComponentTypes, type EquipmentSlot, type Player } from "@minecraft/server";
 
+export const swapItems = (playerA: Player, playerB: Player) => {
+    const inventoryA = playerA.getComponent(EntityComponentTypes.Inventory).container;
+    const inventoryB = playerB.getComponent(EntityComponentTypes.Inventory).container;
+
+    for (let i = 0; i < inventoryA.size; i++) {
+        const itemA = inventoryA.getItem(i);
+        const itemB = inventoryB.getItem(i);
+
+        if (itemA && itemB) {
+            inventoryA.swapItems(i, i, inventoryB);
+        } else if (itemA) {
+            inventoryA.moveItem(i, i, inventoryB);
+        } else if (itemB) {
+            inventoryB.moveItem(i, i, inventoryA);
+        }
+    }
+};
+
 export const swapEquipment = (playerA: Player, playerB: Player, slot: EquipmentSlot): void => {
     const componentA = playerA.getComponent(EntityComponentTypes.Equippable);
     const componentB = playerB.getComponent(EntityComponentTypes.Equippable);
